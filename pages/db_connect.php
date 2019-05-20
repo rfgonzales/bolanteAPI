20103e483941c7081c254b14bb9c566fc7dc6e98
Fritz Gonzales <rfgonzales12@gmail.com>
2019-05-20 21:04:46 +0800

    fixxed Db

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -1,8 +1,8 @@
 
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);

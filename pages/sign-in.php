683476615dc1c9f2799940282f151bf883703f45
Fritz Gonzales <rfgonzales12@gmail.com>
2019-05-20 20:59:23 +0800

    Try

--- pages/sign-in.php
+++ pages/sign-in.php
@@ -2,6 +2,10 @@
 <html>
 
 <head>
+<?php 
+include_once ('con.php');
+
+?>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>Sign In | Bootstrap Based Admin Template - Material Design</title>

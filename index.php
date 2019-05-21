736a4b4ef18b0238a3150c17bbee1d9b46ec40b4
Fritz Gonzales <rfgonzales12@gmail.com>
2019-05-22 03:38:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -43,12 +43,7 @@ else{
 
 
 ?>
-<?php
-include 'db_connection.php';
-$conn = OpenCon();
-echo "Connected Successfully";
-CloseCon($conn);
-?>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
